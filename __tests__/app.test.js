de70ec517ca75829891e3e4d8d0b96c87cf8974e
Alejae1998 <alejagr1998@gmail.com>
2022-08-08 15:58:23 -0700

    adding test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,34 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
-describe('backend-express-template routes', () => {
+
+const mockUser = {
+  fisrtName: 'Test',
+  lastName: 'User',
+  email: 'ale@test.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({...mockUser, ...userProps });
+
+  const { email } = user;
+  await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+  return [agent, user];
+};
+
+describe('top-secrets routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('post create new user', async () => {
+    const res = await (await request(app).post('/api/v1/users')).send(mockUser);
+    expect(res.body.email).toBe('ale@test.com');
   });
   afterAll(() => {
     pool.end();
